a2f28b1b0ebed591758a3a8dfa5117be4dcfc74e
Isuru <isuru.mwijesingha@gmail.com>
2021-09-29 20:49:26 +0530

    code cleanup

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,34 +1,17 @@
 export const functional = {
-  tangerine: '#FF8946',
-  tangerineTint20: 'rgba(255, 137, 70, 0.2)',
   kiwi: '#16AD77',
   bloodRed: '#DB1515',
-  blueSkyTint20: 'rgba(59, 123, 220, 0.2)',
-  curtain: 'rgba(0, 0, 0, 0.6)',
 };
 
 export const foundation = {
   pewter: '#5C677C',
   watermelon: '#FF5A61',
-  watermelon10: '#FFEFED',
   white: '#FFFFFF',
   tin: '#D5D8DF',
-  black: '#000000',
   aluminum: '#F7F7F7',
   stone: '#939BAB',
   satin: '#2E3C57',
-  satinTint50: 'rgba(46,60,87,0.5)',
-  background: '#F2F2F2',
-  background2: '#E5E5E5',
-  warhead: '#00809C',
-  marble: '#EBEEF2',
   grape: '#7C2AF7',
-  grape15: '#E6E2F5',
   grape40: '#997DFF',
-  SystemBlueLight: '#007AFF',
   eggplant: '#2E046C',
-};
-
-export const utility = {
-  purps: '#9108D1',
 };
